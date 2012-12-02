7b3e0eb5b72138168c0df2be13582f9c6f64690b
Brian Groenke <30ksoftware@usa.com>
2012-12-01 21:53:08 -0500

    Updated copyrights

--- applet-src/org/madeirahs/applet/ui/PageView.java
+++ applet-src/org/madeirahs/applet/ui/PageView.java
@@ -1,3 +1,24 @@
+/*
+ *  The MHS-Collections Project applet contains the projects primary front-end
+ *  code deployed on the website for use by end-users.
+ *  Copyright © 2012  Madeira Historical Society (developed by Brian Groenke)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  Note: This class was borrowed from the Groenke Commons Java API with full permission from the author.
+ */
+
 package org.madeirahs.applet.ui;
 
 import java.awt.*;
